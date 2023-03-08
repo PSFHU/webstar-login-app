0b629d93517c22112bed32dac1487776b9c868db
PSFHU <prokszamano@gmail.com>
2023-03-08 13:07:27 +0100

    feat: :sparkles: Character navigation implemented

--- src/app/character-details/character-details.component.ts
+++ src/app/character-details/character-details.component.ts
@@ -9,6 +9,8 @@ import { CharacterService } from '../character.service';
 })
 export class CharacterDetailsComponent {
   characters: Character[] = [];
+  activeCharacter?: Character;
+  activeCharacterIndex = 0;
 
   constructor(private characterService: CharacterService) {}
 
@@ -17,10 +19,33 @@ export class CharacterDetailsComponent {
   }
 
   getCharacters() {
-    this.characterService
-      .getCharacters()
-      .subscribe(
-        (responseCharacters) => (this.characters = responseCharacters)
-      );
+    this.characterService.getCharacters().subscribe((responseCharacters) => {
+      this.characters = responseCharacters;
+      this.activeCharacter = this.characters[this.activeCharacterIndex];
+    });
+  }
+
+  goForward() {
+    this.stepCharacter(+1);
+  }
+  goBack() {
+    this.stepCharacter(-1);
+  }
+
+
+  refreshActiveCharacter(){
+    this.activeCharacter = this.characters[this.activeCharacterIndex];
+  }
+  jumpTo(characterIndex: number) {
+    if (this.characters[characterIndex]) {
+      this.activeCharacterIndex = characterIndex;
+      this.refreshActiveCharacter();
+    }
+  }
+  stepCharacter(amount: number) {
+    if (this.characters[this.activeCharacterIndex + amount]) {
+      this.activeCharacterIndex += amount;
+      this.refreshActiveCharacter();
+    }
   }
 }
